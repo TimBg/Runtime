86d71ba3489924cb5b6f61e82797aa081eea5fba
Tymofii Bohatyrov <timofiy@nxte.nl>
2023-02-25 04:21:27 +0300

    add simple jsEngine

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const JSParser  = require('./parser.js');
+const JSParser = require('./parser.js');
+const JSEngine = require('./jsEngine.js'); 
 
 const jsParser = new JSParser();
+const jsEngine = new JSEngine();
 
 const jsCode = "1 + 2 * 3";
-const astBinaryTreeRoot = jsParser.parse(jsCode);
 
-console.log(astBinaryTreeRoot);
+const astBinaryTreeRoot = jsParser.parse(jsCode);
+const byteCode = jsEngine.getByteCode(astBinaryTreeRoot);
+
+console.log(byteCode);
---
+++ jsEngine.js
@@ -0,0 +1,39 @@
+class JSEngine {
+    static operatorToOpCode = {
+        "+": 0,
+        "-": 1,
+        "*": 2,
+        "/": 3,
+        "%": 4,
+        "**": 5,
+    };
+
+    getByteCode(ast) {
+        return this.generateByteCode(ast).join('');
+    }
+      
+    generateByteCode(ast) {
+        if (ast.type === 'BinaryOperator') {
+            const leftBytecode = this.generateByteCode(ast.leftOperand);
+            const rightBytecode = this.generateByteCode(ast.rightOperand);
+
+            let operatorCode = JSEngine.operatorToOpCode[ast.operator];
+      
+            return [
+                ...leftBytecode,
+                ...rightBytecode,
+                operatorCode,
+            ];
+        }
+        
+        if (ast.type === 'Literal') {
+            const LOAD_CONST = 6;
+            
+            return [parseInt(ast.value), LOAD_CONST];
+        }
+
+        throw new Error(`Unknown node type: ${ast.type}`);
+      }
+}
+
+module.exports = JSEngine;
